d7115f62d1705d71416ce81758f143db9605df50
Mousse bathily <bathilymousse24@gmail.com>
2025-07-02 00:16:08 +0200

    Create firebaseConfig.js
    // Remplace par ta config Firebase ici
    export const firebaseConfig = {
      apiKey: "TON_API_KEY",
      authDomain: "TON_AUTH_DOMAIN",
      projectId: "TON_PROJECT_ID",
      storageBucket: "TON_STORAGE_BUCKET",
      messagingSenderId: "TON_MESSAGING_SENDER_ID",
      appId: "TON_APP_ID",
    };

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Remplace par ta config Firebase ici
+export const firebaseConfig = {
+  apiKey: "TON_API_KEY",
+  authDomain: "TON_AUTH_DOMAIN",
+  projectId: "TON_PROJECT_ID",
+  storageBucket: "TON_STORAGE_BUCKET",
+  messagingSenderId: "TON_MESSAGING_SENDER_ID",
+  appId: "TON_APP_ID",
+};
